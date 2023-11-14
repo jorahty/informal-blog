994840fe033a4685754d08f44b890da415dfdb74
James Tennant <jtennant@ucsc.edu>
2023-11-13 23:28:39 -0800

    remove footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,8 @@ import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
 
 export default function Footer() {
+  return <></>
+
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
