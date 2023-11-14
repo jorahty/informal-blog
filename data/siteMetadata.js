af76c80c681f2657cab1353b4d023f8e4f21ae19
James Tennant <jtennant@ucsc.edu>
2023-11-13 23:49:07 -0800

    fix ts error

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,25 +18,25 @@ const siteMetadata = {
   // youtube: 'https://youtube.com/@jorahty',
   // linkedin: 'https://linkedin.com/in/jorahty',
   locale: 'en-US',
-  // analytics: {
-  //   // If you want to use an analytics provider you have to add it to the
-  //   // content security policy in the `next.config.js` file.
-  //   // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-  //   umamiAnalytics: {
-  //     // We use an env variable for this site to avoid other users cloning our analytics ID
-  //     umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   },
-  //   // plausibleAnalytics: {
-  //   //   plausibleDataDomain: '', // e.g. blog.jorahty.com
-  //   // },
-  //   // simpleAnalytics: {},
-  //   // posthogAnalytics: {
-  //   //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   // },
-  //   // googleAnalytics: {
-  //   //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-  //   // },
-  // },
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    umamiAnalytics: {
+      // We use an env variable for this site to avoid other users cloning our analytics ID
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. blog.jorahty.com
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // googleAnalytics: {
+    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    // },
+  },
   // newsletter: {
   //   // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
   //   // Please add your .env file and modify it according to your selection
